eb591f044ccd8c809debd2a723afee7ef1755812
Nick Edkins (Home) <nedk437@aucklanduni.ac.nz>
2020-03-17 20:59:52 +1300

    Successful build

---
+++ absoft_run_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "/Users/nickedkins/Dropbox/GitHub Repositories/RRTM-LWandSW-Python-wrapper"
+exec "/Users/nickedkins/Dropbox/GitHub Repositories/RRTM-LWandSW-Python-wrapper/RRTM_SWandLW_Pythonwrapper" 
