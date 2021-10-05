ceb10a1d3a46c8c3517e0af554ab3fe215a3c930
Nick Edkins (Home) <nedk437@aucklanduni.ac.nz>
2021-10-06 12:24:16 +1300

    prrtmlw exec compiles again, creates an output rrtm file

---
+++ PRRTM LW/absoft_run_env.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd "/Users/nickedkins/Home GitHub Repositories/RRTM-LWandSW-Python-wrapper/PRRTM LW"
+exec "/Users/nickedkins/Home GitHub Repositories/RRTM-LWandSW-Python-wrapper/PRRTM LW/prrtmlw" 
